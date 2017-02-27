0213c0b443909fae791f8344ed3d052dd5f1ea05
Viktor Charypar <charypar@gmail.com>
2017-02-27 20:15:32 +0000

    Implemented the first two rule of the game

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -4,3 +4,50 @@
   "Creates a new grid width by height"
   [width height]
   (vec (repeat height (vec (repeat width false)))))
+
+(defn assoc-cell
+  "Sets a cell at coordinates x y (0 0 is top left) in grid to value"
+  [grid x y value]
+  (assoc-in grid [y x] value))
+
+(defn get-cell
+  "Gets a value of cell at coordinates x y (0 0 is top left)"
+  [grid x y]
+  (get-in grid [y x]))
+
+(defn count-neighbours
+  "Count live cells in the 8 immediate neighbours of x y"
+  [grid x y]
+  (let [neighbours [[-1 -1] [-1 0] [-1 1]
+                    [0 -1] [0 1]
+                    [1 -1] [1 0] [1 1]]]
+    (->>
+      neighbours
+      (map
+        (fn [[dx dy]] (get-cell grid (+ x dx) (+ y dy))))
+      (filter true?)
+      (count))))
+
+(defn grid-map
+  "Maps over the grid applying a function of arguments [x y val]"
+  [grid map-fn]
+  (let [w (count grid)
+        h (count (get grid 0))]
+    (vec
+      (map
+        (fn [y]
+          (vec
+            (map
+              (fn [x] (map-fn x y (get-cell grid x y)))
+              (range w))))
+        (range h)))))
+
+(defn game-step
+  "For a given grid returns the next step of the game"
+  [grid]
+  (grid-map grid
+    (fn [x y _]
+      (let [n (count-neighbours grid x y)]
+        (cond
+          (< n 2) false
+          :else true)))))
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -5,16 +5,63 @@
 (deftest new-grid-test
   (testing "creates an empty 2x2 grid"
     (is (=
-          [[false false] [false false]]
+          [[false false]
+           [false false]]
           (new-grid 2 2))))
+
   (testing "creates an empty 3x3 grid"
     (is (=
           [[false false false]
            [false false false]
            [false false false]]
           (new-grid 3 3))))
+
   (testing "creates an empty 1x2 grid"
     (is (=
           [[false]
            [false]]
           (new-grid 1 2)))))
+
+(deftest assoc-cell-test
+  (testing "sets a cell to true"
+    (let [empty-grid (new-grid 2 2)
+          expected-grid [[true false]
+                         [false false]]
+          actual-grid (assoc-cell empty-grid 0 0 true)]
+      (is (= actual-grid expected-grid))))
+
+  (testing "sets a cell to true"
+    (let [empty-grid (new-grid 2 2)
+          expected-grid [[false true]
+                         [false false]]
+          actual-grid (assoc-cell empty-grid 1 0 true)]
+      (is (= actual-grid expected-grid)))))
+
+(deftest count-neighbours-test
+  (testing "a single cell has 0 neighbours"
+    (let [grid [[true false]
+                [false false]]
+          expected 0
+          actual (count-neighbours grid 0 0)]
+      (is (= actual expected)))))
+
+(deftest game-step-test
+  (testing "a single cell dies"
+    (let [grid (assoc-cell (new-grid 2 2) 0 0 true)
+          expected-grid (new-grid 2 2)
+          actual-grid (game-step grid)]
+      (is (= actual-grid expected-grid))))
+  (testing "two cells survive"
+    (let [grid [[true true]
+                [false false]]
+          expected-grid [[false false]
+                         [true true]]
+          actual-grid (game-step grid)]
+      (is (= actual-grid expected-grid))))
+  (testing "three cells survive"
+    (let [grid [[true true]
+                [true false]]
+          expected-grid [[true true]
+                         [true true]]
+          actual-grid (game-step grid)]
+      (is (= actual-grid expected-grid)))))
