c5c227546d646d9a21442c698945eb7745bb1d17
Viktor Charypar <charypar@gmail.com>
2017-02-27 18:29:11 +0000

    GAME. OF. LIFE.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to game-of-life
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/game_of_life/core_test.clj
@@ -0,0 +1,7 @@
+(ns game-of-life.core-test
+  (:require [clojure.test :refer :all]
+            [game-of-life.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
